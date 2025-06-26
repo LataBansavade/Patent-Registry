97529cf5365edede9322d54905bf95db5ecc97bc
lata bansavade <latabansavade24@gmail.com>
2025-06-26 14:21:40 +0530

    removed all frontend part kept only hardhat things

--- Frontend/src/vite-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-
-declare module '@reown/appkit/react' {
-    export * from '@reown/appkit/dist/react';
-  }
-  
-  declare module '@reown/appkit/networks' {
-    export * from '@reown/appkit/dist/networks';
-  }
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -5,7 +5,7 @@ async function main() {
   const patent = await PatentRegistry.deploy()
   await patent.waitForDeployment();
 
-  console.log(`PatentRegistry deployed to: ${patent.target}`); // `target` is the contract address
+  console.log(`PatentRegistry deployed to: ${patent.target}`); 
 
   // PatentRegistry deployed to: 0xE610d1bd643cd32600BF4402CC9E00f4887682Ed
 }
@@ -14,3 +14,10 @@ main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
+
+
+
+// SEPOLIA_RPC_URL=https://sepolia.infura.io/v3/0396cb651a6841589f2f1e0b44f014a6
+// PRIVATE_KEY=a867ba282b6f2f1464da2db2df6804f813962811d3f5151445953938fc5d0695
+// ETHERSCAN_API_KEY=WD4HEIPWMDUH9NJGX8RYWN1196TYRE9RI9
+// VITE_APP_APKIT_PROJECT_ID=9ae66a3a5795776468024f08e1ba4ace
